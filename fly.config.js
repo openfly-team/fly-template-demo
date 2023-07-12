6aade3535e29afa55d801269bfc2763a926fef7c
紫竹 <luozhu2021@gmail.com>
2023-07-12 17:32:37 +0800

    Update fly.config.js

--- fly.config.js
+++ fly.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  prompts: {}
+  prompts: [
+    {
+      type: 'text',
+      name: 'username',
+      message: 'What is your GitHub username?'
+    },
+    {
+      type: 'number',
+      name: 'age',
+      message: 'How old are you?'
+    },
+    {
+      type: 'text',
+      name: 'about',
+      message: 'Tell something about yourself',
+      initial: 'Why should I?'
+    }
+  ]
 }
